65eaf35f89cbe54ee9eddf52d360ad9fa899bf83
Peter Schwarz <p.schwarz1994@outlook.de>
2021-03-31 08:32:00 +0200

    change module

--- Pybot.py
+++ Pybot.py
@@ -1,12 +1,18 @@
 import discord
+from discord.ext import commands
 import os
 
-client = discord.client()
+client = commands.Bot(command_prefix="PyBot")
 
 
 @client.event
 async def on_ready():
     print("Bot ist Bereit!")
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Ping! {client.latency}')
 
 
 client.run(os.environ['DISCORD_TOKEN'])

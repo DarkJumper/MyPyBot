46ec1c9baf122748eb73c4f59f1b6ab8c4ec9fb1
Peter Schwarz <p.schwarz1994@outlook.de>
2021-04-02 12:19:13 +0200

    rename cogs to PyBot

--- bot.py
+++ bot.py
@@ -19,16 +19,16 @@ bot = Bot(command_prefix=fetch_prefix, case_insensitive=True, help_command=None)
 
 @bot.command()
 async def load(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
+    bot.unload_extension(f'PyBot.{extension}')
 
 
 @bot.command()
 async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
+    bot.unload_extension(f'PyBot.{extension}')
 
 
 for filename in os.listdir('./PyBot'):
     if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
+        bot.load_extension(f'PyBot.{filename[:-3]}')
 
 bot.run(os.environ['DISCORD_TOKEN'])

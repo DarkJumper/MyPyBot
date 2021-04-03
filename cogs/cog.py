4b7488f900caeb8b9cbcd0adfe4893ab9cccbf97
Peter Schwarz <p.schwarz1994@outlook.de>
2021-04-03 23:21:11 +0200

    create new commands

--- cogs/cog.py
+++ cogs/cog.py
@@ -5,43 +5,35 @@ class Cog(commands.Cog):
 
     def __init__(self, bot) -> None:
         self.bot = bot
+        """
+         Events Begin!
+        """
 
-    # Event
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Bot ist Online!")
+        print("PyBot ist Online!")
+        """
+        Commands Begin!
+        """
 
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'Ping! {self.bot.latency}')
 
+    @commands.command()
+    async def Hallo(self, ctx):
+        await ctx.send(
+            f'Hallo ich bin PyBot. \nFalls du hilfe Brauchen solltest kannst du mich einfach fragen mit "PyBot help" .'
+            )
+
+    @commands.command()
+    async def Github(ctx):
+        await ctx.send(f'Mein Code findest du hier\n-> https://github.com/DarkJumper/MyPyBot')
+
+    @commands.command()
+    async def help(context):
+        await context.send("help ist noch nicht erstellt worden....sorry!")
+
 
 def setup(bot):
     bot.add_cog(Cog(bot))
-
-
-""" @bot.event
-async def on_ready():
-    print("PyBot ist Bereit!1")
-
-
-@bot.command()
-async def Hallo(ctx):
-    await ctx.send(
-        f'Hallo ich bin PyBot. \nFalls du hilfe Brauchen solltest kannst du mich einfach fragen mit "PyBot help" .'
-        )
-
-
-@bot.command()
-async def erschaffer(ctx):
-    await ctx.send(f'Meinen Erschaffe kenne ich nicht Persönlich.... \n')
-
-
-@bot.command()
-async def code(ctx):
-    await ctx.send(f'Mein Code findest du hier\n-> https://github.com/DarkJumper/MyPyBot')
-
-
-@bot.command()
-async def help(context):
-    await context.send("help ist noch nicht erstellt worden....sorry!") """

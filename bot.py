13dd8b20ced0fe6b1bd9a9a46c577447c62c2371
Peter Schwarz <p.schwarz1994@outlook.de>
2021-04-03 23:12:22 +0200

    try3

--- bot.py
+++ bot.py
@@ -7,16 +7,16 @@ bot = Bot(command_prefix="!", case_insensitive=True, help_command=None)
 
 @bot.command()
 async def load(ctx, extension):
-    bot.unload_extension(f'PyBot.{extension}')
+    bot.unload_extension(f'cogs.{extension}')
 
 
 @bot.command()
 async def unload(ctx, extension):
-    bot.unload_extension(f'PyBot.{extension}')
+    bot.unload_extension(f'cogs.{extension}')
 
 
-for filename in os.listdir('./PyBot'):
+for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        bot.load_extension(f'PyBot.{filename[:-3]}')
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 bot.run(os.environ['DISCORD_TOKEN'])

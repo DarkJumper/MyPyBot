258bd0c2ec202a0161a277518cd1a0d0b5441f3c
Peter Schwarz <p.schwarz1994@outlook.de>
2021-04-06 13:15:02 +0200

    help command

--- cogs/run_code_cog/help.py
+++ cogs/run_code_cog/help.py
@@ -42,13 +42,15 @@ class HelpRunner:
         "zig",
         ]
 
+    @staticmethod
     async def languages(self) -> str:
         lang = ', '.join(f'`{language}`' for language in self.language_support)
-        return "Folgende Sprachen stehen zur verfügung:\n" + lang
+        return await "Folgende Sprachen stehen zur verfügung:\n" + lang
 
+    @staticmethod
     async def get_help(self) -> str:
         formating = """!run ```language\n
         your code\n
         ```
         """
-        return formating
+        return await formating
